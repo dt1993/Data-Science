fe3986d947737167ddbb74aa8e09f32b2a84c924
dt1993 <60934098+dt1993@users.noreply.github.com>
2020-04-25 18:30:57 -0500

    Add python code and data set

---
+++ LSD-Math_Study/life.py
@@ -0,0 +1,13 @@
+import math
+
+theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+    
+def square_root(x):
+    return math.sqrt(x)
+    
+    
+    
+    
